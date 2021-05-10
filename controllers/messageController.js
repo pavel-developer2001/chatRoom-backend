fd84cd9aae851fef960d53e168da5a5780e58740
pavel-developer2001 <gaidukpavel18@gmail.com>
2021-05-10 13:19:23 +0300

    success with added new messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,11 +3,11 @@ const Message = require("../models/message");
 // const io = require("../index");
 class MessageController {
 	async addMessage(req, res) {
-		const { messageText } = req.body;
+		const { messageText, roomId, userId } = req.body;
 		const newMessage = await Message.create({
 			messageText: messageText,
-			userId: 7,
-			roomId: 16,
+			userId: userId,
+			roomId: roomId,
 		});
 		res.json({ message: "Сообщение отправлено", data: newMessage });
 	}
